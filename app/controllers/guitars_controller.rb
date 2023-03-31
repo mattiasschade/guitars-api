fbcaffb976214395bc2204c00c48da903c329c57
Mattias Schade <mattiasschade@gmail.com>
2023-03-31 13:41:06 -0500

    Guitar show

--- app/controllers/guitars_controller.rb
+++ app/controllers/guitars_controller.rb
@@ -4,5 +4,10 @@ class GuitarsController < ApplicationController
     @guitars = Guitar.all
     render :index
   end
+
+  def show
+    @guitar = Guitar.find_by(id: params["id"])
+    render :show
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/guitars" => "guitars#index"
+  get "/guitars/:id" => "guitars#show"
 end
